1a06f162f3e799daedba12bf3e8a4c3e4813971a
komiya <90894519+misya11p@users.noreply.github.com>
2026-01-18 21:56:15 +0900

    Update solution for compute-advantage

--- compute-advantage/compute-advantage.py
+++ compute-advantage/compute-advantage.py
@@ -5,13 +5,12 @@ def compute_advantage(states, rewards, V, gamma):
     Returns: A (NumPy array of advantages)
     """
     # Write code here
-    V = np.array(V)
     G = []
     Gt = 0.0
     for reward in rewards[::-1]:
         Gt = gamma * Gt + reward
         G.append(Gt)
     G = np.array(G[::-1])
-    Vs = V[states]
-    A = G - Vs
+    V = np.array(V)
+    A = G - V[states]
     return A
